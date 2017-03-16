e07027ee8c253945c2e4746bbaf0476e0c9b67df
Laimonas <laimutis.bendikas@gmail.com>
2017-03-16 19:11:56 +0100

    exercises filtered

---
+++ Exercise/DenSorteBog/DenSorteBog/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+// Toolkit namespace
+using SimpleMvvmToolkit;
+
+// Toolkit extension methods
+using SimpleMvvmToolkit.ModelExtensions;
+
+namespace DenSorteBog
+{
+    /// <summary>
+    /// This class extends ViewModelDetailBase which implements IEditableDataObject.
+    /// <para>
+    /// Specify type being edited <strong>DetailType</strong> as the second type argument
+    /// and as a parameter to the seccond ctor.
+    /// </para>
+    /// <para>
+    /// Use the <strong>mvvmprop</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// </summary>
+    public class CustomerViewModel : ViewModelDetailBase<CustomerViewModel, Customer>
+    {
+        #region Initialization and Cleanup
+
+        // Add a member for ICustomerServiceAgent
+        private ICustomerServiceAgent serviceAgent;
+
+        // Default ctor
+        public CustomerViewModel() { }
+
+        // Ctor that accepts ICustomerServiceAgent
+        public CustomerViewModel(ICustomerServiceAgent serviceAgent)
+        {
+            this.serviceAgent = serviceAgent;
+        }
+
+        #endregion
+
+        #region Notifications
+
+        // TODO: Add events to notify the view or obtain data from the view
+        public event EventHandler<NotificationEventArgs<Exception>> ErrorNotice;
+
+        #endregion
+
+        #region Properties
+
+        // TODO: Add properties using the mvvmprop code snippet
+
+        #endregion
+
+        #region Methods
+
+        // Set the model to a new customer
+        public void NewCustomer()
+        {
+            base.Model = serviceAgent.CreateCustomer();
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand NewCustomerCommand
+        {
+            get
+            {
+                return new DelegateCommand(NewCustomer);
+            }
+        }
+
+        #endregion
+
+        #region Completion Callbacks
+
+        // TODO: Optionally add callback methods for async calls to the service agent
+
+        #endregion
+
+        #region Helpers
+
+        // Helper method to notify View of an error
+        private void NotifyError(string message, Exception error)
+        {
+            // Notify view of an error
+            Notify(ErrorNotice, new NotificationEventArgs<Exception>(message, error));
+        }
+
+        #endregion
+    }
+}
---
+++ Exercise/StockPortfolioMonitoring/StockPortfolioMonitoring/StockPortfolioMonitoring/Observer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** GROUP 19
+ * Vi har lavet vores egen Observer, ikke fordi den der allerede eksistere ikke var god nok, men mere fordi vi var i tvivl om vi måtte bruge den eller om vi selv skulle lave en.
+ * Der er lavet en generisk Push Observer klasse, og en subject klasse, som også er generisk.
+ * Observer klassen bruges til at overvåge et/flere subject, og få nofikationer når der sker en ændring. Eftersom det er en push Observer vil det betyde at Observeren vil modtage data fra det subject der sender en nofikation.
+ **/
+namespace HomeMade.Observer
+{
+    interface IObserver_HM<T>
+    {
+        void update(T subject);
+    }
+    
+    class subject_HM<T> 
+    {
+        List<IObserver_HM<T>> Observers_ = new List<IObserver_HM<T>>();
+
+        public subject_HM()
+        {
+
+        }
+
+        public void Attach(IObserver_HM<T> Observer)
+        {
+            Observers_.Add(Observer);
+        }
+
+        public void Detach(IObserver_HM<T> Observer)
+        {
+            Observers_.Remove(Observer);
+        }
+
+        public void Notify(T subject)
+        {
+
+            foreach (IObserver_HM<T> o in Observers_)
+            {
+                o.update(subject);
+            }
+        }
+    }
+}
