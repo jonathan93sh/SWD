acbe4706dcc1c8d93a6389554983e4806da18458
RedInsanity <laimutis.bendikas@gmail.com>
2017-03-17 16:02:30 +0100

    gitignore added

---
+++ Exercise/DenSorteBogDoc/testWPF/DenSorteBog/DenSorteBog/Model/GaeldsposterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SimpleMvvmToolkit;
+
+namespace DenSorteBog.Model
+{
+    public class GaeldsposterModel : ModelBase<GaeldsposterModel>
+    {
+    }
+}
