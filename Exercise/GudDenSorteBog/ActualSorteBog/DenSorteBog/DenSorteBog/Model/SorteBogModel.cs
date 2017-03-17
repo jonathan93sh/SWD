603dbe391b54ff2b149c92db5f4e45707e44774c
jonathan93sh <schouga4rd@gmail.com>
2017-03-17 22:39:51 +0100

    done

--- Exercise/GudDenSorteBog/ActualSorteBog/DenSorteBog/DenSorteBog/Model/SorteBogModel.cs
+++ Exercise/GudDenSorteBog/ActualSorteBog/DenSorteBog/DenSorteBog/Model/SorteBogModel.cs
@@ -1,43 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+
+using DenSorteBog.DAL;
 
 using SimpleMvvmToolkit;
 
-namespace DenSorteBog
+namespace DenSorteBog.Model
 {
     public class SorteBogModel : ModelBase<SorteBogModel>
     {
-        public SorteBogModel()
-        { }
-        public SorteBogModel(SorteBogModel other)
-        {
-            PersonName = other.PersonName;
-            MoneyValue = other.MoneyValue;
+
+        private static ObservableCollection<Person> Persons_ = Repository.ReadMeasurements();
+
+        private ObservableCollection<Person> skyldere_ = new ObservableCollection<Person>();
+
+
+        public ObservableCollection<Person> Persons { get { return Persons_; }
+            set { Persons_ = value; NotifyPropertyChanged<ObservableCollection<Person>>(m => m.Persons); ObservableCollection<Person> PersonsSkylderetmp = Persons; PersonsSkyldere = Persons; }
         }
 
-        private string personName;
-
-        public string PersonName
+        public ObservableCollection<Person> PersonsSkyldere 
         {
-            get { return personName; }
-            set
+            get 
             {
-                personName = value;
-                NotifyPropertyChanged(m => m.PersonName);
+                
+                skyldere_.Clear();
+                foreach (var item in Persons)
+                {
+                    if (item.money < 0.0 && item != null)
+                    {
+                        skyldere_.Add(item);
+                    }
+                }
+                return skyldere_;  
+            }
+            private set
+            {
+                
+                /*skyldere_ = new ObservableCollection<Person>();
+                foreach (var item in value)
+                {
+                    if (item.money < 0.0)
+                    {
+                        skyldere_.Add(item);
+                    }
+                }*/
+                NotifyPropertyChanged<ObservableCollection<Person>>(m => m.PersonsSkyldere);
+                
+            }
+            
+        }
+
+        public void RemovePerson(Person p)
+        {
+            if (p != null)
+            {
+                Persons.Remove(p);
             }
         }
 
-        private int moneyValue;
-
-        public string MoneyValue
+        public void AddNewPerson(Person p)
         {
-            get { return moneyValue.ToString(); }
-            set
-            {
-                moneyValue = int.Parse(value);
-                NotifyPropertyChanged(m => m.MoneyValue);
-            }
+            Persons.Add(p);
+        }
+
+        public void gemDenSorteBog()
+        {
+            Repository.WriteMeasurements(Persons_);
         }
     }
 }
