48a3ae81c4f6f536fd0b2b99a6e104ae0f2c7dfa
Laimonas <laimutis.bendikas@gmail.com>
2017-05-11 14:14:19 +0200

    Klar til aflevering

---
+++ Exercise/Mediator/First example - Mediator/Mediator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediator;
+
+// Laimonas & Jonathan - Gruppe 11, E6SWD.
+
+namespace Mediator_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chatroom = new ConcreteMediator<string>();
+
+            var LifeOfBo = new Colleague("Bo", chatroom);
+            var LifeOfIb = new Colleague("Ib", chatroom);
+            var LifeOfOle = new Colleague("Ole", chatroom);
+
+            LifeOfOle.broadcastMsg("Hey i'm Ole");
+
+            var LifeOfBrian = new Colleague("Brian", chatroom);
+            LifeOfOle.broadcastMsg("any one?");
+
+            LifeOfBrian.broadcastMsg("Hello Ole i'm Brian");
+            LifeOfBrian.leave();
+          
+
+            LifeOfOle.broadcastMsg("Oh Brian leaved :(");
+
+            Console.ReadLine();
+        }
+    }
+
+    class Colleague : IColleague<string>
+    {
+
+        private string _name;
+        IMediator<string> _chatroom;
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public Colleague(string name, IMediator<string> chatroom)
+        {
+            _name = new string(name.ToCharArray());
+            _chatroom = chatroom;
+
+            _chatroom.register(this);
+            Console.WriteLine(_name + ": has enter a chatroom");
+        }
+
+        ~Colleague()
+        {
+            _chatroom.unregister(this);
+            Console.WriteLine(_name + ": has leaved the chatroom");
+        }
+
+        public void leave()
+        {
+            _chatroom.unregister(this);
+            Console.WriteLine(_name + ": has leaved the chatroom");
+        }
+
+        public void receiveMsg(string msg)
+        {
+            Console.WriteLine(_name + " received this message: " + msg);
+        }
+
+
+        public void broadcastMsg(string msg)
+        {
+            Console.WriteLine(_name + ": send this message: " + msg);
+            _chatroom.broadcastMsg(this, msg);
+        }
+    }
+
+}
---
+++ Exercise/Mediator/Second example - chatroom_server/chatroom_server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;  
+
+// Laimonas & Jonathan - Gruppe 11, E6SWD.
+
+namespace chatroom_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // opretter server.
+            var chatserver = new chatroom_server(3000u);
+
+            // starter server.
+            chatserver.startServer();
+
+            while (true) { }
+
+        }
+    }
+}
