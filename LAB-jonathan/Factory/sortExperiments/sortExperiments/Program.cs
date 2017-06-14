cb4c54d7eb75609f6f0678dd867ad0b573fa273c
jonathan93sh <schouga4rd@gmail.com>
2017-06-15 00:08:54 +0200

    init factory

---
+++ LAB-jonathan/Factory/sortExperiments/sortExperiments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IGenerator> generators = new List<IGenerator>();
+            List<ISortAlgorithms> algorithms = new List<ISortAlgorithms>();
+
+            generators.Add(new RandomGenerator((int)DateTime.Now.Ticks));
+            generators.Add(new nearlySortedGenerator(95, (int)DateTime.Now.Ticks));
+            generators.Add(new ReverseOrderGenerator());
+            generators.Add(new FewUniqueRandomOrderGenerator((int)DateTime.Now.Ticks, 5));
+            algorithms.Add(new BubbleSort());
+            algorithms.Add(new InsertionSort());
+            algorithms.Add(new ShellSort());
+            algorithms.Add(new QuickSort());
+
+            Experiment allTest = new Experiment(generators, algorithms, 10000, "The Test");
+
+            allTest.start();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LAB-jonathan/Factory/sortExperiments/sortExperiments/SortExperiment.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortExperiments
+{
+
+    abstract class sortExperiment
+    {
+        protected List<ISortAlgorithms> _Algoritmes;
+        protected List<IGenerator> _Generators;
+        public void start()
+        {
+            Console.WriteLine("Experiment: " + getTitle());
+            
+            foreach(IGenerator generator in _Generators)
+            {
+                setGenerator(generator);
+                Console.WriteLine("current generator: " + generator.getName());
+                int[] array = generate();
+
+                foreach(ISortAlgorithms alg in _Algoritmes)
+                {
+                    setAlgorithm(alg);
+                    Console.WriteLine("current algorithm: " + alg.getName());
+                    
+                    int[] tmpArray = new int[array.Length];
+                    for (int i = 0; i < array.Length; i++)
+                        tmpArray[i] = array[i];
+
+                    Console.WriteLine("unsorted numbers(first 10): ");
+                    for (int i = 0; i < (array.Length < 10 ? array.Length: 10) ; i++)
+                    {
+                        Console.Write(tmpArray[i] + " ");
+                    }
+                    Console.WriteLine();
+                    double time = sort(tmpArray);
+
+                    Console.WriteLine("sorted numbers(first 10), sort time(" + time + " ms) : ");
+                    for (int i = 0; i < (array.Length < 10 ? array.Length : 10); i++)
+                    {
+                        Console.Write(tmpArray[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+        }
+
+        protected abstract void setGenerator(IGenerator gen);
+        protected abstract void setAlgorithm(ISortAlgorithms alg);
+        protected abstract string getTitle();
+        protected abstract int[] generate();
+        protected abstract double sort(int[] array);
+    }
+
+    class Experiment : sortExperiment
+    {
+        private IGenerator _CurrGenerator = null;
+        private SuperSorter _superSorter = new SuperSorter();
+        private int _n;
+        private string _name;
+        public Experiment(List<IGenerator> generator, List<ISortAlgorithms> alg, int n, string name)
+        {
+            _Generators = generator;
+            _Algoritmes = alg;
+            
+            _n = n;
+            _name = name;
+        }
+
+        protected override void setGenerator(IGenerator gen)
+        {
+            if(gen != null)
+            {
+                _CurrGenerator = gen; 
+            }
+        }
+
+        protected override void setAlgorithm(ISortAlgorithms alg)
+        {
+            if(alg != null)
+            {
+                _superSorter.setAlgorithms(alg);
+            }
+        }
+
+        protected override string getTitle()
+        {
+            return _name;
+        }
+
+        protected override int[] generate()
+        {
+            return _CurrGenerator.generate(_n);
+        }
+
+        protected override double sort(int[] array)
+        {
+            return _superSorter.sort(array);
+        }
+    }
+
+
+}
